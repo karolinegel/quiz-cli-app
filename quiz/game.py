cfae562988d719ccbc449eaf20d384122427db45
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-04-03 01:01:24 +0300

    Integrate question shuffle

--- quiz/game.py
+++ quiz/game.py
@@ -4,10 +4,13 @@ def check_answer(user, correct):
     return user.lower() == correct.lower()
 from quiz.questions import get_questions
 
+from quiz.utils import shuffle_questions
+
 def run_game():
     global score
-    for q in get_questions():
-        answer = q["a"]  # имитация ответа
+    questions = shuffle_questions(get_questions())
+    for q in questions:
+        answer = q["a"]
         if check_answer(answer, q["a"]):
             score += 1
     return score
