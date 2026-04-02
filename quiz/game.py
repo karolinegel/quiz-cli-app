ffae97801b6eef99db5747c364f8ab8d8e915f0c
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-04-03 00:58:21 +0300

    Add basic game loop

--- quiz/game.py
+++ quiz/game.py
@@ -2,3 +2,12 @@
 score = 0
 def check_answer(user, correct):
     return user.lower() == correct.lower()
+from quiz.questions import get_questions
+
+def run_game():
+    global score
+    for q in get_questions():
+        answer = q["a"]  # имитация ответа
+        if check_answer(answer, q["a"]):
+            score += 1
+    return score
