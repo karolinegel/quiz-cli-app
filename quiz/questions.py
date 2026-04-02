e225b435551fffdbf6659617c4bdefb108861a3a
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-04-03 01:04:33 +0300

    Extend questions list

--- quiz/questions.py
+++ quiz/questions.py
@@ -5,3 +5,10 @@ questions = [
 ]
 def get_questions():
     return questions
+
+{"q": "5+5?", "a": "10"}
+questions = [
+    {"q": "2+2?", "a": "4"},
+    {"q": "Capital of France?", "a": "Paris"},
+    {"q": "5+5?", "a": "10"}
+]
